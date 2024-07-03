4b4ed41758f2836bde069597e0422be37b9bbcf2
nortosem <82008236+nortosem@users.noreply.github.com>
2024-07-03 20:28:35 +0000

    only the kv model
    FossilOrigin-Name: af19937d9a78c5578286ffbcad1f9c74e854c2a95a3e02f3328605686916e43f

---
+++ litestash/models.py
@@ -0,0 +1,20 @@
+"""The Models
+
+The base and table models for a stash.
+"""
+from datetime import datetime
+from sqlmodel import Field
+from sqlmodel import Relationship
+from sqlmodel import SQLModel
+from litestash.utils import mk_table_names
+
+class StashModel(SQLModel):
+    """Stash: Base Model
+
+    TODO
+    """
+    id: int | None = Field(default=None, primary_key=True)
+    hash_key: str | None = Field(default=None, primary_key=True)
+    key: str = Field(default=None, index=True)
+    value: str = Field(default=None, index=True)
+    date_created: datetime | None = Field(default=None)
