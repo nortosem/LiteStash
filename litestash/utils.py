f1347d6c6a77a6d09ae314f4e7bcb2e07e122cd2
nortosem <82008236+nortosem@users.noreply.github.com>
2024-07-05 02:42:32 +0000

    moved mk tables out to the model module to localize use
    FossilOrigin-Name: 18eaaa9082c867819c190dc80b85b9352b8afbe2560ffcddcc6e33d50a938966

--- litestash/utils.py
+++ litestash/utils.py
@@ -2,19 +2,4 @@
 
 
 """
-from litestash.config import TableName
-from litestash.config import Num
-from litestash.config import LowerCase
-from litestash.config import UpperCase
 
-def mk_table_names():
-    """Make Table Names
-
-    Generate names for all tables in cache
-    Return a generator.
-    """
-    for i in [Num,LowerCase,UpperCase]:
-        for table_name in (
-            f'{TableName.ROOT.value}{suffix.value}' for suffix in i
-        ):
-            yield table_name
