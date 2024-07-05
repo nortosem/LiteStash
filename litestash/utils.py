1e577ae593fffefef6f392d40a0c38fcda29f455
nortosem <82008236+nortosem@users.noreply.github.com>
2024-07-05 20:39:45 +0000

    added setup utils, column utils
    FossilOrigin-Name: b8236cec9b945042565dc6ce277fc4c65452e3a7809a3fc0bef75f8921441c09

--- litestash/utils.py
+++ litestash/utils.py
@@ -2,4 +2,119 @@
 
 
 """
+from litestash.config import TableName
+from litestash.config import Num
+from litestash.config import LowerCase
+from litestash.config import UpperCase
+from litestash.config import SetupDB
+from litestash.config import ColumnSetup
+from litestash.models import StashColumns
+from sqlalchemy.schema import Table
+from sqlalchemy.schema import Metadata
+from sqlalchemy import Integer
+from sqlalchemy import Text
+from sqlalchemy import JSON
+from typing import Generator
 
+def setupDB_file(db: str | None):
+    """Setup Database file
+
+    Prepare default cache file, or custom name.
+    TODO: Path customize && check
+    """
+    pass
+
+
+def setupDB_memory():
+    """Setup in memory temporary database."""
+    pass
+
+
+def setupDB(db: str | None):
+    """Handle correct setupDB function call"""
+    if db is None:
+        return setupDB_memory()
+    return setupDB_file(db)
+
+
+def mk_id() -> Column:
+    """Return the unique id column"""
+    return StashColumns.column(
+        ColumnSetup.ROW_ID.value,
+        Integer,
+        primary_key=True
+    )
+
+
+def mk_hash() -> Column:
+    """Return a Column for the hash"""
+    return     return StashColumns.column(
+        ColumnSetup.HASH.value,
+        Text,
+        index=True,
+        unique=True,
+        nullable=False
+    )
+
+
+def mk_key() -> Column:
+    """Return a Column for the key being stored."""
+    return StashColumns.column(
+        ColumnSetup.KEY.value,
+        Text,
+        unique=True,
+        index=True,
+        nullable=False
+    )
+
+
+def mk_value() -> Column:
+    """Return a Column for the value being stored."""
+    return StashColumns.column(
+        ColumnSetup.VALUE.value,
+        JSON,
+        nullable-False
+    )
+
+
+def mk_date_created() -> Column:
+    """Return a Column for the date the data was added."""
+    return StashColumns.get_column(
+        ColumnSetup.DATE_CREATED.value,
+        Integer,
+        primary_key=True
+    )
+
+
+def mk_columns() -> Generator[Column, None, None]:
+    """Make Columns
+
+    Return a generator for all columns used in each table.
+    """
+    for column in (
+        mk_id(),
+        mk_hash(),
+        mk_key(),
+        mk_val(),
+        mk_date_created()
+    ):
+        yield column
+
+
+def mk_table_names() -> Generator[str, None, None]:
+    """Make all valid Table names
+
+    Generate names for all tables in cache
+    Return a generator.
+    """
+    for chars in (Num,LowerCase,UpperCase):
+        for suffix in chars:
+           yield f'{TableName.ROOT.value}{suffix.value}'
+
+
+def mk_tables() -> Generator[Table, None, None]:
+    """
+
+
+    """
+    pass
