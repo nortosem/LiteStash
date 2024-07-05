f8e37896527548460b3b31eacdde66a37cb295c1
jcy <>
2024-07-05 19:46:20 +0000

    refactored to only manage the data models. lints 10/10.  Test Todo.
    FossilOrigin-Name: bcbbd9fa01e94646c7c19d6b8ee19ff79ad64280873b95dc37247f0f5cb82ef7

--- litestash/models.py
+++ litestash/models.py
@@ -2,32 +2,22 @@
 
 The base and table models for a stash.
 """
-from litestash.config import TableName
-from litestash.config import Num
-from litestash.config import LowerCase
-from litestash.config import UpperCase
 from typing import Literal
-from typing import Generator
 from datetime import datetime
-from pydantic import BaseModel
 from pydantic import Json
 from pydantic import StrictStr
 from pydantic import StrictInt
 from pydantic import Field
-from pydantic import Relationship
 from pydantic.dataclasses import dataclass
-from sqlalchemy.schema import Column
-from sqlalchemy.schema import Table
-from sqlalchemy.shema import Metadata
 from sqlalchemy import Integer
-from sqlalchemy import String
+from sqlalchemy import Text
 from sqlalchemy import JSON
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.schema import Column
+from typing import Literal
 
-Base = declarative_base()
 
 @dataclass(slots=True)
-class LiteStashData(BaseModel):
+class LiteStashData:
     """The LiteStash Data
 
     This class defines a class of data for use with the LiteStash database.
@@ -37,7 +27,7 @@ class LiteStashData(BaseModel):
 
 
 @dataclass(slots=True)
-class LiteStashStore(Base, LiteStashData):
+class LiteStashStore(LiteStashData):
     """LiteStash Database Fields
 
     The database storage class.  Defines all columns and column types in db.
@@ -45,77 +35,34 @@ class LiteStashStore(Base, LiteStashData):
     """
     hash_key: StrictStr | None = Field(default=None, unique=True, index=True)
     key: StrictStr = Field(default=None, unique=True, index=True)
-    value: JSON = Field(default=None, index=True)
-    date_created:  | None = Field(default=datetime.now())
+    value: JSON = Field(default=None)
+    date_created: datetime | None = Field(default=datetime.now())
 
 
 @dataclass(slots=True)
-class ColumnLiteStash(BaseModel):
-    """LiteStash Columns
+class LiteStasher:
+    """Valid LiteStash Column
 
+    Definition for sqlite database columns.
+    The DateTime is unix time int over now().
     TODO
     """
-    name: StrictStr
-    type_: Literal["StrictStr", "StrictInt", "JSON"]
+    name: str
+    type_: Literal[Text,Integer,JSON]
     primary_key: bool = False
     index: bool = False
     unique: bool = False
+    nullable: bool = False
 
-
-    def mk_id(self) -> Column:
-        """Return the unique id column"""
-        return Column('id',Integer,primary_key=True)
-
-    def mk_hash(self) -> Column:
-        """Return a Column for the hash"""
-        return Column(self.name, str, index=True, unique=True)
-
-    def mk_key(self) -> Column:
-        """Return a Column for the key being stored."""
-        return Column(self.name, str, index=True, unique=True)
-
-    def mk_value(self) -> Column:
-        """Return a Column for the value being stored."""
-        return Column(self.name, JSON, index=True)
-
-    def mk_date_create(self) -> Column:
-        """Return a Column for the date the data was added."""
-        return Column(self.name, str)
-
-
-def mk_table_names() -> Generator[str, None, None]:
-    """Make all valid Table names
-
-    Generate names for all tables in cache
-    Return a generator.
-    """
-    for chars in (Num,LowerCase,UpperCase):
-        for suffix in chars:
-           yield f'{TableName.ROOT.value}{suffix.value}'
-
-
-def mk_table(name: str,  ) -> Table:
-    """
-
-
-    """
-    pass
-
-
-def mk_tables() -> Generator[Table, None, None]:
-    """
-
-
-    """
-    pass
-
-
-def mk_columns() -> Generator[Column, None, None]:
-    """
-
-
-    """
-    pass
-
+    def column(self) -> Column:
+        """Create columns for database tables"""
+        return Column(
+            self.name,
+            self.type_,
+            primary_key=self.primary_key,
+            index=self.index,
+            unique=self.unique,
+            nullable=self.nullable
+        )
 
 
