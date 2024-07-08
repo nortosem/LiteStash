3cd17e7cee023926dc2c2c70c00119d1373ea89f
nortosem <82008236+nortosem@users.noreply.github.com>
2024-07-08 21:37:43 +0000

    updated imports, LiteStash init rebuild commence LiteStash{Meta,Engine,Session} minimal setup almost done
    FossilOrigin-Name: 9f8532727b1c6ee3d38d7c3f7efe773d4060aabe12a2b7089c94e10ce952983b

--- litestash/store.py
+++ litestash/store.py
@@ -4,17 +4,20 @@ Define the LiteStash key-value database object.
 LiteStash is a text key with JSON value key value store.
 """
 from pathlib import Path
+from collections import namedtuple
 #from litestash.model import
 from litestash.utils import mk_hash
+from litestash.utils import setup_engine
 from litestash.utils import hash_key
+from litestash.utils import mk_tables
 from litestash.config import Pragma
 from litestash.config import StashSlots
 from litestash.config import MetaSlots
+from litestash.confim import EngineStash
 from sqlalchemy import event
+from sqlalchemy.engine import Engine
 from sqlalchemy.schema import Metadata
 from sqlalchemy.orm import Session
-from sqlalchemy import create_engine
-from sqlalchemy.engine import Engine
 from pydantic import ValidationError
 
 class LiteStash:
@@ -28,17 +31,22 @@ class LiteStash:
                  StashSlots.DB_SESSION.value
     )
 
-    def __init__(self,
-            db_name: str | None,
-            db_path: Path | None
-        ):
+    def __init__(self):
         """Initiate a new LiteStash
 
         Creates a new empty cache by default.
         """
         self.engine = LiteStashEngine()
-        self.metadata = LiteStashMeta()
+        self.metadata = build_db(self.engine)
         self.db_session = LiteStashSession()
+
+    def build_db(self, engine: LiteStashEngine):
+        """Initiate all database files.
+
+        Arg:
+            engine (LiteStashEngine): The engines for the stash
+        """
+        return(LiteStashMeta(engine))
 
     def set(self, key: , value: ) -> None:
         """LiteStash Key Setter
@@ -125,29 +133,41 @@ class LiteStashMeta:
                  MetaSlots.WZU.value,
                 )
 
-    def __init__(self):
+    def __init__(self, engine_stash: LiteStashEngine):
         """LiteStash Metadata __init__
 
         Create fresh empty metadata objects for all of the databases
         """
-        self.zfn = Metadata()
-        self.fnn = Metadata()
-        self.ael = Metadata()
-        self.fil = Metadata()
-        self.jml = Metadata()
-        self.nrl = Metadata()
-        self.svl = Metadata()
-        self.wzl = Metadata()
-        self.aeu = Metadata()
-        self.fiu = Metadata()
-        self.jmu = Metadata()
-        self.nru = Metadata()
-        self.svu = Metadata()
-        self.wzu = Metadata()
+        self.zfn = bond_to_engine(engine_stash.get_engine(MetaSlots.ZFN.value))
+        self.fnn = bond_to_engine(engine_stash.get_engine(MetaSlots.FNN.value))
+        self.ael = bond_to_engine(engine_stash.get_engine(MetaSlots.AEN.value))
+        self.fil = bond_to_engine(engine_stash.get_engine(MetaSlots.FIL.value))
+        self.jml = bond_to_engine(engine_stash.get_engine(MetaSlots.JML.value))
+        self.nrl = bond_to_engine(engine_stash.get_engine(MetaSlots.NRL.value))
+        self.svl = bond_to_engine(engine_stash.get_engine(MetaSlots.SVL.value))
+        self.wzl = bond_to_engine(engine_stash.get_engine(MetaSlots.EZL.value))
+        self.aeu = bond_to_engine(engine_stash.get_engine(MetaSlots.AEU.value))
+        self.fiu = bond_to_engine(engine_stash.get_engine(MetaSlots.FIU.value))
+        self.jmu = bond_to_engine(engine_stash.get_engine(MetaSlots.JMU.value))
+        self.nru = bond_to_engine(engine_stash.get_engine(MetaSlots.NRU.value))
+        self.svu = bond_to_engine(engine_stash.get_engine(MetaSlots.SVU.value))
+        self.wzu = bond_to_engine(engine_stash.get_engine(MetaSlots.WZU.value))
+
+    def bond_to_engine(self, engine: Engine) -> Metadata:
+        """Bond with specific engine
+
+        Used to specify a bind to an engine and create tables for that engine.
+
+        Args (Engine): The engine to create tables for
+        """
+        metadata = Metadata()
+        metadata = mk_tables(metadata)
+        metadata.create_all(bind=engine, checkfirst=True)
+        return metadata
 
     def __iter__(self):
         """Iterator for all database metadata"""
-        yield from self.__slots__
+        yield from (getattr(self, slot) for slot in self.__slots__)
 
     def __repr__(self):
         """Metadata Official Representation
@@ -198,12 +218,47 @@ class LiteStashEngine:
                  MetaSlots.WZU.value,
                 )
 
+
     def __init__(self):
-        pass
+        """Default DB & Engine setup"""
+        StashEngine = namedtuple(EngineStash.TYPE_NAME.value,
+                                 [EngineStash.DB_NAME.value,
+                                  EngineStash.ENGINE.value
+                                 ]
+                                )
+        self.zfn = StashEngine(*setup_engine(MetaSlots.ZFN.value))
+        self.fnn = StashEngine(*setup_engine(MetaSlots.FNN.value))
+        self.ael = StashEngine(*setup_engine(MetaSlots.AEL.value))
+        self.fil = StashEngine(*setup_engine(MetaSlots.FIL.value))
+        self.jml = StashEngine(*setup_engine(MetaSlots.JML.value))
+        self.nrl = StashEngine(*setup_engine(MetaSlots.NRL.value))
+        self.svl = StashEngine(*setup_engine(MetaSlots.SVL.value))
+        self.wzl = StashEngine(*setup_engine(MetaSlots.WZL.value))
+        self.aeu = StashEngine(*setup_engine(MetaSlots.AEU.value))
+        self.fiu = StashEngine(*setup_engine(MetaSlots.FIU.value))
+        self.jmu = StashEngine(*setup_engine(MetaSlots.JMU.value))
+        self.nru = StashEngine(*setup_engine(MetaSlots.NRU.value))
+        self.svu = StashEngine(*setup_engine(MetaSlots.SVU.value))
+        self.wzu = StashEngine(*setup_engine(MetaSlots.WFU.value))
+
+
+    def get_engine(self, name: str):
+        """Get DB Engine by name
+
+        Args:
+            name (str): database filename & db engine
+        """
+        if hasattr(self, name):
+            return getattr(getattr(self, name),
+                            EngineStash.ENGINE.value
+                            )
+        else:
+            raise ValueError(f'{EngineStash.VALUE_ERROR.value}')
+
 
     def __iter__(self):
         """Iterator for all database engines"""
-        yield from self.__slots__
+        yield from (getattr(self, slot) for slot in self.__slots__)
 
     def __repr__(self):
         """"""
@@ -237,12 +292,39 @@ class LiteStashSession:
                  MetaSlots.WZU.value,
                 )
 
-    def __init__(self):
-        pass
+    def __init__(self, engine_stash: LiteStashEngine):
+        """Default init
+
+        Given an instance of the LiteStashEngine match
+        all
+        """
+        self.zfn =
+        self.fnn = getattr(engine_stash, MetaSlots.FNN.value)
+        self.ael = getattr(engine_stash, MetaSlots.AEL.value)
+        self.fil = getattr(engine_stash, MetaSlots.FIL.value)
+        self.jml = getattr(engine_stash, MetaSlots.JML.value)
+        self.nrl = getattr(engine_stash, MetaSlots.NRL.value)
+        self.svl = getattr(engine_stash, MetaSlots.SVL.value)
+        self.wzl = getattr(engine_stash, MetaSlots.WZL.value)
+        self.aeu = getattr(engine_stash, MetaSlots.AEU.value)
+        self.fiu = getattr(engine_stash, MetaSlots.FIU.value)
+        self.jmu = getattr(engine_stash, MetaSlots.JMU.value)
+        self.nru = getattr(engine_stash, MetaSlots.NRU.value)
+        self.svu = getattr(engine_stash, MetaSlots.SVU.value)
+        self.wzu = getattr(engine_stash, MetaSlots.WFU.value)
+
+
+    def mk_session(self, engine_stash: LiteStashEngine) -> Session:
+        """Make a sesssion
+
+        Given a LiteStashEngine instance make a session factory
+        for each database engine.
+        """
+        getattr(engine_stash, MetaSlots.ZFN.value)
 
     def __iter__(self):
         """Iterator for all database session factories"""
-        yield from self.__slots__
+        yield from (getattr(self, slot) for slot in self.__slots__)
 
     def __repr__(self):
         """"""
