aa48319db3f9b152aff2d783eb824c750153e92c
nortosem <82008236+nortosem@users.noreply.github.com>
2024-07-11 21:32:14 +0000

    linted refactor for core/util.py done
    FossilOrigin-Name: e9d6131d9c5c31b32c9a19a0d004bfdbc17e847d5020605a8ca6e48ee34f9f1b

--- litestash/utils.py
+++ litestash/utils.py
@@ -7,155 +7,22 @@ Functions:
     check_key
 
 """
-from collections import namedtuple
 from hashlib import blake2b
 from litestash.config import Utils
 from litestash.config import Names
 from litestash.config import Digitables
 from litestash.config import LowerTables
-from litestash.config import UpperTables
-from litestash.config import SetupDB
-from litestash.config import ColumnSetup as Col
-from litestash.config import EngineStash
-from litestash.config import MetaStash
-from litestash.config import SessionStash
 from litestash.config import DataScheme
-from litestash.config import FTS5
+from litestash.config import UpperTables
+from litestash.config import ColumnSetup as Col
 from litestash.models import StashColumns
-from sqlalchemy.orm.session import sessionmaker
-from sqlalchemy import inspect
 from sqlalchemy import Table
 from sqlalchemy import Column
-from sqlalchemy import MetaData
-from sqlalchemy import Engine
 from sqlalchemy import Integer
 from sqlalchemy import JSON
 from sqlalchemy import BLOB
-from sqlalchemy import text
-from sqlalchemy import create_engine
+from sqlalchemy import MetaData
 from typing import Generator
-from typing import Tuple
-
-def setup_engine(name: str) -> Engine:
-    """Setup engine
-
-    Args:
-        engine_name (str): match with sqlite.db filename
-
-    Return a tuple of (name, engine)
-    """
-    return (name,
-            create_engine(
-                f'{SetupDB.sqlite()}{SetupDB.dirname()}{name}.db',
-                echo=SetupDB.echo.value
-                )
-            )
-
-StashEngine = namedtuple(
-    EngineStash.TYPE_NAME.value,
-    [EngineStash.DB_NAME.value,
-    EngineStash.ENGINE.value]
-)
-StashEngine.__doc__ = EngineStash.DOC.value
-
-
-def setup_metadata(*args):
-    """Setup Metadata & Tables
-
-    Args:
-        stash (LiteStashEngine):  Retrieve name & engine to setup from
-        slot (str): datable named attribute slot
-    """
-    name, engine = args
-    metadata = MetaData()
-    metadata = mk_tables(metadata)
-    metadata.create_all(bind=engine, checkfirst=True)
-    return (name, metadata, engine)
-
-
-StashMeta = namedtuple(
-    MetaStash.TYPE_NAME.value,
-    [MetaStash.DB_NAME.value,
-    MetaStash.METADATA.value]
-)
-StashMeta.__doc__ = MetaStash.DOC.value
-
-
-def setup_fts(*args):
-    """Setup Full Text Search
-
-    Given the engine stash and metadata stash:
-        Add the FTS5 virtual tbles and connections to the database.
-    """
-    name, metadata, engine = data
-    with engine.connect() as connection:
-        for table in metadata.sorted_tables:
-            fts = f'{FTS5.TABLE_PREFIX.value}{table.name}'
-            connection.execute(text(f"""
-                {FTS5.MK_TABLE.value} {fts} {FTS5.USING.value} {FTS5.OPEN.value}
-                    {Col.KEY.value}, {Col.VALUE.value}, {Col.TIME.value},
-                    {FTS5.CONTENT.value}{table.name},
-                    {FTS5.ROW_ID.value}{Col.HASH.value}
-                {FTS5.CLOSE.value}
-            """))
-            connection.execute(text(f"""
-                {FTS5.MK_TRIGGER.value} {table.name}_ai
-                {FTS5.AFTER_INSERT.value} {table.name}
-                {FTS5.BEGIN_INSERT.value} {fts}(
-                    {Col.KEY.value}, {Col.VALUE.value}, {Col.TIME.value},
-                ) {FTS5.VALUES.value} (
-                    {FTS5.NEW.value}{Col.KEY.value},
-                    {FTS5.NEW.value}{Col.VALUE.value},
-                    {FTS5.NEW.value}{Col.TIME.value},
-                    );
-                {FTS5.END.value}
-                {FTS5.MK_TRIGGER.value} {table.name}_ai
-                {FTS5.AFTER_UPDATE.value} {table.name}
-                {FTS5.BEGIN_INSERT.value} {fts}(
-                    {Col.KEY.value}, {Col.VALUE.value}, {Col.TIME.value},
-                ) {FTS5.VALUES.value} (
-                    {FTS5.NEW.value}{Col.KEY.value},
-                    {FTS5.NEW.value}{Col.VALUE.value},
-                    {FTS5.NEW.value}{Col.TIME.value},
-                    );
-                {FTS5.END.value}
-                {FTS5.MK_TRIGGER.value} {table.name}_ai
-                {FTS5.AFTER_DELETE.value} {table.name}
-                {FTS5.BEGIN_INSERT.value} {fts}(
-                    {Col.KEY.value}, {Col.VALUE.value}, {Col.TIME.value},
-                ) {FTS5.VALUES.value} (
-                    {FTS5.NEW.value}{Col.KEY.value},
-                    {FTS5.NEW.value}{Col.VALUE.value},
-                    {FTS5.NEW.value}{Col.TIME.value},
-                    );
-                {FTS5.END.value}
-            """))
-        return (name, metadata)
-
-
-def setup_sessions(*args):
-    """Make a sesssion
-
-    Given a LiteStashEngine make a session factory for a database engine.
-
-    Args:
-        slot (str): database name slot
-        stash (LiteStashEngine): An Engine with Metadata already setup
-    """
-    name, engine = args
-    if inspect(engine).get_table_name():
-        session = sessionmaker(engine)
-    else:
-        raise ValueError(f'{SessionStash.VALUE_ERROR.value}')
-    return (name, session)
-
-
-StashSession = namedtuple(
-    SessionStash.TYPE_NAME.value,
-    [SessionStash.DB_NAME.value,
-    SessionStash.SESSION.value]
-)
-StashSession.__doc__ = SessionStash.DOC.value
 
 def check_key(key: str) -> bytes:
     """Validates and encodes an ASCII string key to bytes."""
@@ -358,7 +225,7 @@ def wzu_db() -> Generator[bytes,None,None]:
     ):
         yield l
 
-def get_db_name(hcar: bytes) -> bytes:
+def get_db_name(char: bytes) -> bytes:
     """Find database for given char"""
     match char:
         case char if char in zf_db():
