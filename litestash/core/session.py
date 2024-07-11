d2fb6f301c00019b4c5c6cf4b8b64be961382bea
jcy <>
2024-07-11 20:34:49 +0000

    mi
    FossilOrigin-Name: a485262ccdb017fa288719c992deaca9815cee8da92b8ae0e2860e9283c445dd

---
+++ litestash/core/session.py
@@ -0,0 +1,126 @@
+"""The LiteStash Session Module
+
+This module provide the core session module for creating a LiteStash.
+Every instance of a LiteStash has a session attribute to access the sessions
+associated with that instance.
+
+This class is intended for use in a LiteStash:
+    def __init__(self):
+        self.session = Session
+"""
+from litestash.core.engine import Engine
+from litestash.config import MetaSlots
+from litestash.utils import StashSession
+
+class Session:
+    """The Session Manager
+
+    All databases have a dedicated session factory.
+    The LiteStashSession class encapsulates the creation and access to these
+    factories.
+    #TODO: finish docs
+    """
+    __slots__ = (MetaSlots.ZFD.value,
+                 MetaSlots.FND.value,
+                 MetaSlots.AEL.value,
+                 MetaSlots.FIL.value,
+                 MetaSlots.JML.value,
+                 MetaSlots.NRL.value,
+                 MetaSlots.SVL.value,
+                 MetaSlots.WZL.value,
+                 MetaSlots.AEU.value,
+                 MetaSlots.FIU.value,
+                 MetaSlots.JMU.value,
+                 MetaSlots.NRU.value,
+                 MetaSlots.SVU.value,
+                 MetaSlots.WZU.value,
+                )
+
+    def __init__(self, lite_stash_engine: LiteStashEngine):
+        """Default init
+
+        TODO: docs
+        """
+        self.zfd = StashSession(
+            *setup_sessions(
+                getattr(lite_stash_engine, MetaSlots.ZFD.value)
+            )
+        )
+        self.fnd = StashSession(
+            *setup_sessions(
+                getattr(lite_stash_engine, MetaSlots.FND.value)
+            )
+        )
+        self.ael = StashSession(
+            *setup_sessions(
+                getattr(lite_stash_engine, MetaSlots.AEL.value)
+            )
+        )
+        self.fil = StashSession(
+            *setup_sessions(
+                getattr(lite_stash_engine, MetaSlots.FIL.value)
+            )
+        )
+        self.jml = StashSession(
+            *setup_sessions(
+                getattr(lite_stash_engine, MetaSlots.JML.value)
+            )
+        )
+        self.nrl = StashSession(
+            *setup_sessions(
+                getattr(lite_stash_engine,
+                MetaSlots.NRL.value)
+            )
+        )
+        self.svl = StashSession(
+            *setup_sessions(
+                getattr(lite_stash_engine, MetaSlots.SVL.value)
+            )
+        )
+        self.wzl = StashSession(
+            *setup_sessions(
+                getattr(lite_stash_engine, MetaSlots.WZL.value)
+            )
+        )
+        self.aeu = StashSession(
+            *setup_sessions(
+                getattr(lite_stash_engine, MetaSlots.AEU.value)
+            )
+        )
+        self.fiu = StashSession(
+            *setup_sessions(
+                getattr(lite_stash_engine, MetaSlots.FIU.value)
+            )
+        )
+        self.jmu = StashSession(
+            *setup_sessions(
+                getattr(lite_stash_engine, MetaSlots.JMU.value)
+            )
+        )
+        self.nru = StashSession(
+            *setup_sessions(
+                getattr(lite_stash_engine, MetaSlots.NRUvalue
+            )
+        )
+        self.svu = StashSession(
+            *setup_sessions(
+                getattr(lite_stash_engine, MetaSlots.SVU.value)
+            )
+        )
+        self.wzu = StashSession(
+            *setup_sessions(
+                getattr(lite_stash_engine, MetaSlots.WZU.value)
+            )
+        )
+
+    def __iter__(self):
+        """Iterator for all database session factories"""
+        yield from (getattr(self, slot) for slot in self.__slots__)
+
+    def __repr__(self):
+        """TODO"""
+        pass
+
+    def __str__(self):
+        """TODO"""
+        pass
