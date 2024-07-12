b4ddcd483e35a0de7ae90e104df59ad4d5130a39
nortosem <82008236+nortosem@users.noreply.github.com>
2024-07-12 02:17:37 +0000

    added config module for core metadata utilities
    FossilOrigin-Name: 0647d839d948e3f679e75459b623082711bdeab3bafe6f882fdb1c3645859760

---
+++ litestash/core/config/schema.py
@@ -0,0 +1,50 @@
+"""The LiteStash Core Metadata Config
+
+TODO: docs
+"""
+from enum import Enum
+
+class Valid(Enum):
+    """Valid Root"""
+    pass
+
+
+class MetaSlots(Valid):
+    """Slots for the LiteStashMetadata"""
+    ZFD = 'zfd'
+    FND = 'fnd'
+    AEL = 'ael'
+    FIL = 'fil'
+    JML = 'jml'
+    NRL = 'nrl'
+    SVL = 'svl'
+    WZL = 'wzl'
+    AEU = 'aeu'
+    FIU = 'fiu'
+    JMU = 'jmu'
+    NRU = 'nru'
+    SVU = 'svu'
+    WZU = 'wzu'
+
+
+class Pragma(Valid):
+    """Sqlite Pragma
+
+    The default pragma configuration.
+    """
+    PRAGMA = 'PRAGMA'
+    JOURNAL_MODE = 'journal_mode=WAL'
+    SYNCHRONOUS = 'synchronous=NORMAL'
+    FOREIGN_KEYS = 'foreign_keys=ON'
+
+    @staticmethod
+    def journal_mode() -> str:
+        return f'{Pragma.PRAGMA.value} {Pragma.JOURNAL_MODE.value}'
+
+    @staticmethod
+    def synchronous() -> str:
+        return f'{Pragma.PRAGMA.value} {Pragma.SYNCHRONOUS.value}'
+
+    @staticmethod
+    def foreign_keys() -> str:
+        return f'{Pragma.PRAGMA.value} {Pragma.FOREIGN_KEYS.value}'
