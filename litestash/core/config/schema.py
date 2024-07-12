2db1134f7065742bb955b1b2491546cf6a275932
nortosem <82008236+nortosem@users.noreply.github.com>
2024-07-12 02:30:49 +0000

    updated imports, added MetaData and db schema config
    FossilOrigin-Name: 3854c93a28437779cfe243e6e765d14f6688176cb7bcc65b6e5a30085e28d1f8

--- litestash/core/config/schema.py
+++ litestash/core/config/schema.py
@@ -2,12 +2,7 @@
 
 TODO: docs
 """
-from enum import Enum
-
-class Valid(Enum):
-    """Valid Root"""
-    pass
-
+from litestash.config import Valid
 
 class MetaSlots(Valid):
     """Slots for the LiteStashMetadata"""
@@ -48,3 +43,77 @@ class Pragma(Valid):
     @staticmethod
     def foreign_keys() -> str:
         return f'{Pragma.PRAGMA.value} {Pragma.FOREIGN_KEYS.value}'
+
+
+class ColumnSetup(Valid):
+    """The Column Setup
+
+    Define the column attributes for each table
+    """
+    HASH = 'key_hash'
+    KEY = 'key'
+    VALUE = 'value'
+    TIME = 'timedate'
+
+
+class Names(Valid):
+    """Database filenames
+
+    Default filenames for each database.
+    Also the Table name suffix as HASH
+    """
+    HASH = '_hash'
+    ZFD = b'zfd.db'
+    FND = b'fnd.db'
+    AEL = b'ael.db'
+    FIL = b'fil.db'
+    JML = b'jml.db'
+    NRL = b'nrl.db'
+    SVL = b'svl.db'
+    WZL = b'wzl.db'
+    AEU = b'ael.db'
+    FIU = b'fiu.db'
+    JMU = b'jmu.db'
+    NRU = b'nru.db'
+    SVU = b'svu.db'
+    WZU = b'wzu.db'
+
+    @staticmethod
+    def db_zft():
+        """Filename for database with hashes that start with 0-4"""
+        return f'{Names.ZFD.value.decode()}'
+
+    @staticmethod
+    def db_fnt():
+        """Filename for database with hashes that start with 5-9"""
+        return f'{Names.FND.value.decode()}'
+
+    @staticmethod
+    def db_ael():
+        """Filename for database with hashes that start with a-e"""
+        return f'{Names.AEL.value.decode()}'
+
+    @staticmethod
+    def db_fil():
+        """Filename for database with hashes that start with f-i"""
+        return f'{Names.FIL.value.decode()}'
+
+    @staticmethod
+    def db_jml():
+        """Filename for database with hashes that start with j-m"""
+        return f'{Names.JML.value.decode()}'
+
+    @staticmethod
+    def db_nrl():
+        """Filename for database with hashes that start with n-r"""
+        return f'{Names.NRL.value.decode()}'
+
+    @staticmethod
+    def db_svl():
+        """Filename for database with hashes that start with s-v"""
+        return f'{Names.SVL.value.decode()}'
+
+    @staticmethod
+    def db_wzl():
+        """Filename for database with hashes that start with w-z"""
+        return f'{Names.WZL.value.decode()}'
