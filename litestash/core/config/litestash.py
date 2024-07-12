2a920ff5a7343804028998cc235d285a0b4c7607
jcy <>
2024-07-12 02:31:57 +0000

    updated imports, added engine config
    FossilOrigin-Name: f1e6ec19d3a095852fd1749c8fed44df9492658a818d2e3dff597136f5f810fb

--- litestash/core/config/litestash.py
+++ litestash/core/config/litestash.py
@@ -2,10 +2,11 @@
 
 #TODO doc
 """
+from litestash.config import Valid
 
 class EngineAttr(Valid):
     """The namedtuple config for all engine attributes of a LiteStash"""
-    TYPE_NAME = 'StashEngine'
+    TYPE_NAME = 'EngineAttr'
     DB_NAME = 'db_name'
     ENGINE = 'engine'
     DOC = '''Defines a named tuple for tuple returned by utils.setup_engine.
@@ -18,15 +19,52 @@ class EngineAttr(Valid):
 
 class MetaAttr(Valid):
     """The namedtuple config for all metadata attributes of a LiteStash"""
-    TYPE_NAME = 'StashMeta'
-    DB_NAME = f'{EngineStash.DB_NAME.value}'
+    TYPE_NAME = 'MetaAttr'
+    DB_NAME = f'{EngineAttr.DB_NAME.value}'
     METADATA = 'metadata'
     DOC = '''todo'''
 
 class SessionAttr(Valid):
     """The namedtuple config for all session attribues of a LiteStash """
-    TYPE_NAME = 'StashSession'
-    DB_NAME = f'{EngineStash.DB_NAME.value}'
+    TYPE_NAME = 'SessionAttr'
+    DB_NAME = f'{EngineAttr.DB_NAME.value}'
     SESSION = 'session'
     VALUE_ERROR = 'Invalid database: no tables found'
     DOC = '''todo'''
+
+
+class EngineConf(Valid):
+    """The Engine Config
+
+    Provide the configuation to setup a database engine.
+    """
+    SQLITE = 'sqlite://'
+    DIR_NAME = 'litestash'
+    ECHO = True
+    FUTURE = True
+    NO_ECHO = False
+    NO_FUTURE = False
+
+    @staticmethod
+    def sqlite() -> str:
+        return f'{EngineConf.SQLITE.value}'
+
+    @staticmethod
+    def dirname() -> str:
+        return f'{EngineConf.DIR_NAME.value}'
+
+    @staticmethod
+    def echo() -> str:
+        return f'{EngineConf.ECHO.value}'
+
+    @staticmethod
+    def future() -> str:
+        return f'{EngineConf.FUTURE.value}'
+
+    @staticmethod
+    def no_echo() -> str:
+        return f'{EngineConf.NO_ECHO.value}'
+
+    @staticmethod
+    def no_future() -> str:
+        return f'{EngineConf.NO_FUTURE.value}'
