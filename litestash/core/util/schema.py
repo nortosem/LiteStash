668af6c53cf7780ed79bdd4cf8eee394f0ca791e
nortosem <82008236+nortosem@users.noreply.github.com>
2024-07-12 16:23:31 +0000

    module refactor
    FossilOrigin-Name: 0fa254bda26540531762e207b80945e373c43c304ac1cf8030269c87e22bb214

---
+++ litestash/core/util/schema.py
@@ -0,0 +1,29 @@
+"""The LiteStash Core Metadata Utilities
+
+#TODO docs
+"""
+
+
+def mk_table_names() -> Generator[str, None, None]:
+    """Make all valid Table names
+
+    Generate names for all tables in cache
+    Return a generator.
+    """
+    for chars in (Digitables,LowerTables,UpperTables):
+        for char in chars:
+            yield f'{char.value}{Names.HASH.value}'
+
+
+def mk_tables(metadata: MetaData) -> MetaData:
+    """Make Tables
+
+    Create all tables using preset columns and names.
+    """
+    for table_name in mk_table_names():
+        Table(
+            table_name,
+            metadata,
+            *(column for column in mk_columns())
+        )
+    return metadata
