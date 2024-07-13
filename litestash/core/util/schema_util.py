71cb96a178316e4be1643056faeb5ef7b8d8f696
nortosem <82008236+nortosem@users.noreply.github.com>
2024-07-13 23:32:24 +0000

    on massive db no distributed across 14 db files, db name checkes
    FossilOrigin-Name: 1603c95388739ad2f6cfc55e08d4e185799392c83c583c4892740d793839591c

--- litestash/core/util/schema_util.py
+++ litestash/core/util/schema_util.py
@@ -2,34 +2,87 @@
 
 #TODO docs
 """
-from litestash.core.config.schema_conf import Names
+from litestash.core.config.schema_conf import MetaSlots
 from litestash.core.config.tables.digits import Digitables
 from litestash.core.config.tables.lowercase import LowerTables
 from litestash.core.config.tables.uppercase import UpperTables
 from litestash.core.config.litestash_conf import Utils
 from litestash.core.util.table_util import mk_columns
+from litestash.core.util.table_util import create_zfd_tables
+from litestash.core.util.table_util import create_fnd_tables
+from litestash.core.util.table_util import create_ael_tables
+from litestash.core.util.table_util import create_fil_tables
+from litestash.core.util.table_util import create_jml_tables
+from litestash.core.util.table_util import create_nrl_tables
+from litestash.core.util.table_util import create_svl_tables
+from litestash.core.util.table_util import create_wzl_tables
+from litestash.core.util.table_util import create_aeu_tables
+from litestash.core.util.table_util import create_fiu_tables
+from litestash.core.util.table_util import create_jmu_tables
+from litestash.core.util.table_util import create_nru_tables
+from litestash.core.util.table_util import create_svu_tables
+from litestash.core.util.table_util import create_wzu_tables
 from litestash.core.util import prefix_util
 from sqlalchemy import MetaData
 from sqlalchemy import Table
 from typing import Generator
 
-def mk_table_names() -> Generator[str, None, None]:
-    """Make all valid Table names
+def mk_table_names(db_name: str):
+    """Make valid Table names
 
-    Generate names for all tables in cache
+    Generate names for all tables in the given database name.
     Return a generator.
     """
-    for chars in (Digitables,LowerTables,UpperTables):
-        for char in chars:
-            yield f'{char.value}{Names.HASH.value}'
+    if db_name == MetaSlots.ZFD.value:
+        return create_zfd_tables()
+
+    elif db_name == MetaSlots.FND.value:
+        return create_fnd_tables()
+
+    elif db_name == MetaSlots.AEL.value:
+        return create_ael_tables()
+
+    elif db_name == MetaSlots.FIL.value:
+        return create_fil_tables()
+
+    elif db_name == MetaSlots.JML.value:
+        return create_jml_tables()
+
+    elif db_name == MetaSlots.NRL.value:
+        return create_nrl_tables()
+
+    elif db_name == MetaSlots.SVL.value:
+        return create_svl_tables()
+
+    elif db_name == MetaSlots.WZL.value:
+        return create_wzl_tables()
+
+    elif db_name == MetaSlots.AEU.value:
+        return create_aeu_tables()
+
+    elif db_name == MetaSlots.FIU.value:
+        return create_fiu_tables()
+
+    elif db_name == MetaSlots.JMU.value:
+        return create_jmu_tables()
+
+    elif db_name == MetaSlots.NRU.value:
+        return create_nru_tables()
+
+    elif db_name == MetaSlots.SVU.value:
+        return create_svu_tables()
+
+    elif db_name == MetaSlots.WZU.value:
+        return create_wzu_tables()
 
 
-def mk_tables(metadata: MetaData) -> MetaData:
+def mk_tables(db_name: str, metadata: MetaData) -> MetaData:
     """Make Tables
 
     Create all tables using preset columns and names.
     """
-    for table_name in mk_table_names():
+    for table_name in mk_table_names(db_name):
+        print(f'tablename: {table_name}')
         Table(
             table_name,
             metadata,
