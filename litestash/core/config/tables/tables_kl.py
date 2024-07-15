31c286f00d1ec9b178ad9c0ee09d8defd7dba35c
jcy <>
2024-07-15 17:25:53 +0000

    linted
    FossilOrigin-Name: c28f9474e3b801a550c0c802e0da3f14101cb505f7dd79e411def623dd29e614

--- litestash/core/config/tables/tables_kl.py
+++ litestash/core/config/tables/tables_kl.py
@@ -5,7 +5,7 @@ Enumerate the valid chars for keys with hash[:0] equal to k,K,l,L.
 from litestash.core.config.root import Valid
 from litestash.core.config.schema_conf import Names
 
-class Tables_kL(Valid):
+class TablesKL(Valid):
     """Enumeration with access methods"""
     K_LOW = 'k'
     L_LOW = 'l'
@@ -16,21 +16,21 @@ class Tables_kL(Valid):
     def get_table_name(char: str) -> str:
         """Match on char and return table name"""
         match char:
-            case Tables_kL.K_LOW.value:
-                return Tables_kL.k_low()
-            case Tables_kL.L_LOW.value:
-                return Tables_kL.l_low()
-            case Tables_kL.K_UP.value:
-                return Tables_kL.k_upper()
-            case Tables_kL.L_UP.value:
-                return Tables_kL.l_upper()
+            case TablesKL.K_LOW.value:
+                return TablesKL.k_low()
+            case TablesKL.L_LOW.value:
+                return TablesKL.l_low()
+            case TablesKL.K_UP.value:
+                return TablesKL.k_upper()
+            case TablesKL.L_UP.value:
+                return TablesKL.l_upper()
             case _:
                 raise ValueError(Names.ERROR.value)
 
     @staticmethod
     def k_low() -> str:
         """Get the full table name for hash[k:]"""
-        return str(Tables_kL.K_LOW.value
+        return str(TablesKL.K_LOW.value
                    +Names.LOW.value
                    +Names.HASH.value
                    )
@@ -38,7 +38,7 @@ class Tables_kL(Valid):
     @staticmethod
     def l_low() -> str:
         """Get the full table name for hash[l:]"""
-        return str(Tables_kL.L_LOW.value
+        return str(TablesKL.L_LOW.value
                    +Names.LOW.value
                    +Names.HASH.value
                    )
@@ -46,7 +46,7 @@ class Tables_kL(Valid):
     @staticmethod
     def k_upper() -> str:
         """Get the full table name for hash[K:]"""
-        return str(Tables_kL.K_UP.value
+        return str(TablesKL.K_UP.value
                    +Names.UP.value
                    +Names.HASH.value
                    )
@@ -54,7 +54,7 @@ class Tables_kL(Valid):
     @staticmethod
     def l_upper() -> str:
         """Get the full table name for hash[L:]"""
-        return str(Tables_kL.L_UP.value
+        return str(TablesKL.L_UP.value
                    +Names.UP.value
                    +Names.HASH.value
                    )
