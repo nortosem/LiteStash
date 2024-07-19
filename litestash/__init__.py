2632396d90df706a1e40552be496a20cdb8d4d57
jcy <>
2024-07-19 02:20:08 +0000

    __version__ = "beta01-0.1.0"
    FossilOrigin-Name: b9ba7f7325398543b844e90a61ea0fea3d8a7031b1d579cb9488d6e8b71029b2

--- litestash/__init__.py
+++ litestash/__init__.py
@@ -13,6 +13,7 @@ from litestash.models import LiteStashData
 from litestash.models import LiteStashStore
 from litestash.store import LiteStash
 
+__version__ = "beta01-0.1.0"
 __all__ = [
     Main.CORE.value,
     Main.DATA.value,
