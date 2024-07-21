54fb13ed11ae10646ec3dd9e3caab6b370bafcea
jcy <>
2024-07-21 01:06:18 +0000

    fix,clean,update,add prefix tests
    FossilOrigin-Name: 1662f124a45d048f9f274af14ee5e7bf2cc61fd655dd8cfcda4ad6451dcb4fdd

--- tests/core/config/test_schem_conf.py
+++ tests/core/config/test_schem_conf.py
@@ -6,21 +6,22 @@ from litestash.core.config.schema_conf import (
 def test_pragma_values():
     assert Pragma.CONNECT.value == 'connect'
     assert Pragma.PRAGMA.value == 'PRAGMA'
-    assert Pragma.JOURNAL_MODE.value == 'journal_mode=WAL'
-    assert Pragma.SYNCHRONOUS.value == 'synchronous=NORMAL'
-    assert Pragma.FOREIGN_KEYS.value == 'foreign_keys=ON'
+    assert Pragma.JOURNAL_MODE.value == 'journal_mode=WAL;'
+    assert Pragma.SYNCHRONOUS.value == 'synchronous=NORMAL;'
+    assert Pragma.FOREIGN_KEYS.value == 'foreign_keys=ON;'
 
 def test_pragma_methods():
-    assert Pragma.journal_mode() == 'PRAGMA journal_mode=WAL'
-    assert Pragma.synchronous() == 'PRAGMA synchronous=NORMAL'
-    assert Pragma.foreign_keys() == 'PRAGMA foreign_keys=ON'
+    assert Pragma.journal_mode() == 'PRAGMA journal_mode=WAL;'
+    assert Pragma.synchronous() == 'PRAGMA synchronous=NORMAL;'
+    assert Pragma.foreign_keys() == 'PRAGMA foreign_keys=ON;'
+    assert Pragma.valid_json() == 'PRAGMA json_valid = 1;'
 
 def test_column_setup_values():
     assert ColumnSetup.HASH.value == 'key_hash'
     assert ColumnSetup.KEY.value == 'key'
     assert ColumnSetup.VALUE.value == 'value'
     assert ColumnSetup.TIMESTAMP.value == 'timestamp'
-    assert ColumnSetup.MS_TIME.value == 'microseconds'
+    assert ColumnSetup.MICROSECOND.value == 'microsecond'
 
 def test_column_config_values():
     assert ColumnConfig.TYPE_NAME.value == 'ColumnTypes'
