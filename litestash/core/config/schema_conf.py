c96cf5fbbacb86d5caae6d7e76d71fa76f5a11fb
nortosem <82008236+nortosem@users.noreply.github.com>
2024-07-26 23:39:05 +0000

    fts5 value search implemented, TODO: unit testing
    FossilOrigin-Name: 0e75d0166ab05170c575dc4d5a707afc9e73d30c43ec1ddf1dc6d6aaadb55f64

--- litestash/core/config/schema_conf.py
+++ litestash/core/config/schema_conf.py
@@ -45,6 +45,199 @@ class Pragma(Valid):
         return f'{Pragma.PRAGMA.value} {Pragma.JSON.value}'
 
 
+class ViewSetup(Valid):
+    """The View Setup
+
+    Enumerates the SQL used for creating a full table view in each database.
+
+    Members:
+        CREATE: the create sql statement (if not exists is the default)
+        AS: the SQL 'as' term
+        ALL_VALUES: the name of the view of all table values in each database
+    """
+    CREATE = 'CREATE VIEW IF NOT EXISTS'
+    ALL_VALUES = 'all_values'
+
+    @staticmethod
+    def create():
+        return f'{ViewSetup.CREATE.value}'
+
+
+    @staticmethod
+    def all_values():
+        return f'{ViewSetup.ALL_VALUES.value}'
+
+class SearchSetup(Valid):
+    """FTS Setup
+
+    Enumerations for setup of the FTS5 search table.
+
+    Members:
+        CREATE:
+        USING:
+        ALL_VALUES:
+        UNINDEXED:
+        DETAIL:
+        INSERT:
+        UPDATE:
+        DELETE:
+        SQLITE_DIALECT:
+    """
+    CREATE = 'CREATE VIRTUAL TABLE IF NOT EXISTS'
+    USING = 'USING'
+    ALL_VALUES = 'fts_all_values'
+    UNINDEXED = 'UNINDEXED'
+    DETAIL = 'detail=none'
+    INSERT = 'after_insert'
+    UPDATE = 'after_update'
+    DELETE = 'after_delete'
+    SQLITE_DIALECT = 'sqlite'
+
+    @staticmethod
+    def table_name():
+        return f'{SearchSetup.ALL_VALUES.value}'
+
+    @staticmethod
+    def create():
+        return f'{SearchSetup.CREATE.value}'
+
+    @staticmethod
+    def using():
+        return f'{SearchSetup.USING.value}'
+
+    @staticmethod
+    def unindexed():
+        return f'{SearchSetup.UNINDEXED.value}'
+
+    @staticmethod
+    def detail():
+        return f'{SearchSetup.DETAIL.value}'
+
+    @staticmethod
+    def insert():
+        return f'{SearchSetup.INSERT.value}'
+
+    @staticmethod
+    def update():
+        return f'{SearchSetup.UPDATE.value}'
+
+    @staticmethod
+    def delete():
+        return f'{SearchSetup.DELETE.value}'
+
+    @staticmethod
+    def sqlite_dialect():
+        return f'{SearchSetup.SQLITE_DIALECT.value}'
+
+
+class Sql(Valid):
+    """SQL Terms
+
+    Enumerations of regularly used SQL terms.
+    Members:
+        BEGIN:
+        INSERT:
+        DELETE:
+        AND:
+        WHERE:
+        VALUES:
+        NEW:
+        OLD:
+        END:
+    """
+    BEGIN = 'BEGIN'
+    INSERT = 'INSERT INTO'
+    DELETE = 'DELETE FROM'
+    AND = 'AND'
+    WHERE = 'WHERE'
+    VALUES = 'VALUES'
+    NEW = 'NEW'
+    OLD = 'OLD'
+    END = 'END;'
+
+    @staticmethod
+    def begin():
+        return f'{Sql.BEGIN.value}'
+
+    @staticmethod
+    def insert():
+        return f'{Sql.INSERT.value}'
+
+    @staticmethod
+    def delete():
+        return f'{Sql.DELETE.value}'
+
+    @staticmethod
+    def where():
+        return f'{Sql.WHERE.value}'
+
+    @staticmethod
+    def values():
+        return f'{Sql.VALUES}'
+
+    @staticmethod
+    def new():
+        return f'{Sql.NEW.value}'
+
+    @staticmethod
+    def old():
+        return f'{Sql.OLD.value}'
+
+    @staticmethod
+    def end():
+        return f'{Sql.END.value}'
+
+
+class Trigger(Valid):
+    """Trigger SQL
+
+    The SQL components for DDL triggers in sqlite.
+
+    Members:
+        CREATE:
+        AFTER_INSERT:
+        NAME_INSERT:
+        AFTER_UPDATE:
+        NAME_UPDATE:
+        AFTER_DELETE:
+        NAME_DELETE:
+    """
+    CREATE = 'CREATE TRIGGER IF NOT EXISTS'
+    AFTER_INSERT = 'AFTER INSERT ON'
+    NAME_INSERT = '_insert_trigger'
+    AFTER_UPDATE = 'AFTER UPDATE ON'
+    NAME_UPDATE = '_update_trigger'
+    AFTER_DELETE = 'AFTER DELETE ON'
+    NAME_DELETE = '_delete_trigger'
+
+    @staticmethod
+    def create():
+        return f'{Trigger.CREATE.value}'
+
+    @staticmethod
+    def after_insert():
+        return f'{Trigger.AFTER_INSERT.value}'
+
+    @staticmethod
+    def name_insert():
+        return f'{Trigger.NAME_INSERT.value}'
+
+    @staticmethod
+    def after_update():
+        return f'{Trigger.AFTER_UPDATE.value}'
+
+    @staticmethod
+    def name_update():
+        return f'{Trigger.NAME_UPDATE.value}'
+
+    @staticmethod
+    def after_delete():
+        return f'{Trigger.AFTER_DELETE.value}'
+
+    @staticmethod
+    def name_delete():
+        return f'{Trigger.NAME_DELETE.value}'
+
 
 class ColumnSetup(Valid):
     """The Column Setup
@@ -58,6 +251,29 @@ class ColumnSetup(Valid):
     MICROSECOND = 'microsecond'
     TABLE_NAME = 'table_name'
 
+    @staticmethod
+    def hash():
+        return f'{ColumnSetup.HASH.value}'
+
+    @staticmethod
+    def key():
+        return f'{ColumnSetup.KEY.value}'
+
+    @staticmethod
+    def value():
+        return f'{ColumnSetup.VALUE.value}'
+
+    @staticmethod
+    def timestamp():
+        return f'{ColumnSetup.TIMESTAMP.value}'
+
+    @staticmethod
+    def microsecond():
+        return f'{ColumnSetup.MICROSECOND.value}'
+
+    @staticmethod
+    def table_name():
+        return f'{ColumnSetup.TABLE_NAME.value}'
 
 class ColumnConfig(Valid):
     """The namedtuple Column config
--- litestash/core/util/fts.py
+++ litestash/core/util/fts.py
@@ -2,23 +2,49 @@
 
 The Full-Text Search module provides the fts search capability for each
 database. Each value and time column is usable for fts in a functional way.
+def search_all_tables(search_term):
+    with sqlite3.connect('your_database.db') as conn:
+        cursor = conn.cursor()
+        cursor.execute(
+
+            SELECT rowid, table_name
+            FROM all_values_fts
+            WHERE all_values_fts MATCH ?
+           ,
+            (search_term,)
+        )
+
+        results = cursor.fetchall()
+        return results
 """
+from typing import Generator
+from sqlalchemy import Column
+from sqlalchemy import DDL
+from sqlalchemy import event
+from sqlalchemy import select
+from sqlalchemy import text
+from sqlalchemy import union_all
 from litestash.core.config.root import Tables as DB_Names
 from litestash.core.fts_model import ValueSearchColumn
-from litestash.core.config.schema_conf import ColumnSetup as Col
+from litestash.core.config.schema_conf import Sql
+from litestash.core.config.schema_conf import ViewSetup as View
+from litestash.core.config.schema_conf import SearchSetup as Search
+from litestash.core.config.schema_conf import Trigger
+from litestash.core.config.schema_conf import ColumnSetup as C
 from litestash.core.config.schema_conf import ColumnConfig as Conf
 from litestash.core.util.litestash_util import EngineAttributes
 from litestash.core.util.litestash_util import MetaAttributes
 from litestash.core.util.litestash_util import SessionAttributes
+from litestash.core.util.schema_util import mk_table_names
 
-def search(text: str = None) ->:
-    """"""
-    pass
+#def search(text: str = None):
+#    """Search all values in LitesStash for some text string"""
+#    pass
 
 
-def db_search(db_name: str, text: str) ->:
-    """Full-text search of the values stored in the given database"""
-    pass
+#def db_search(db_name: str, text: str):
+#    """Full-text search of the values stored in the given database"""
+#    pass
 
 
 def create_all_search_tables(
@@ -26,49 +52,192 @@ def create_all_search_tables(
     meta_stash: MetaAttributes,
     session_stash: SessionAttributes,
 ) -> None:
-    """Create FTS Tables in each database"""
-    for database in DB_NAMES:
-        pass
+    """Iterate through each database and create the FTS table"""
+    for database in DB_Names:
 
+        engine_detail = engine_stash.get(database)
+        metadata_detail = meta_stash.get(database)
+        session_detail = session_stash.get(database)
+
+        create_view_all_table(
+            engine_detail,
+            metadata_detail,
+            session_detail
+        )
+
+        create_search_table(
+            engine_detail,
+            metadata_detail,
+            session_detail
+        )
 
 
 def create_search_table(
-    db_name: str,
-    db_engine: Engine,
-    db_metadata: MetaData,
-    db_session: Session
+    engine_detail: EngineAttributes = None,
+    metadata_detail: MetaAttributes = None,
+    session_detail: SessionAttributes = None
 ) -> None:
-    """Create a FTS table on the view table for all tables in a database."""
-    pass
+    """
+    Create a virtual FTS table on the view table for all tables in a database.
+    """
+    if engine_detail or metadata_detail or session_detail is None:
+        raise ValueError('details missing error: TODO')
+
+    if (engine_detail.db_name != metadata_detail.db_name or
+            engine_detail.db_name != session_detail.db_name or
+            metadata_detail.db_name != session_detail.db_name):
+        raise ValueError('database must be the same error: TODO')
+
+    create_view_all_table(engine_detail, metadata_detail, session_detail)
+    metadata = metadata_detail.metadata
+    session = session_detail.session
+
+    with session() as mk_fts_session:
+        create_fts_table_statement = text(
+            f"""
+            {Search.CREATE.value} {Search.ALL_VALUES.value}
+            {Search.USING.value} fts5(
+                {C.HASH.value} {Search.UNINDEXED.value},
+                {C.TABLE_NAME.value} {Search.UNINDEXED.value},
+                {C.DATA_VALUE.value},
+                {Search.DETAIL.value}
+            );
+            """
+        )
+        mk_fts_session.execute(create_fts_table_statement)
+        mk_fts_session.commit()
+
+    def _insert_ddl(table_name) -> DDL:
+        insert_statement = DDL(
+            f"""
+            {Trigger.create()} {table_name}{Trigger.name_insert()}
+            {Trigger.after_insert()} {table_name}
+            {Sql.begin()}
+                {Sql.insert()} {Search.table_name()}(
+                    {C.hash()}, {C.table_name()}, {C.value()}
+                )
+                {Sql.values()} (
+                    {Sql.new()}.{C.hash()},
+                    '{table_name}',
+                    {Sql.new()}.{C.value()}
+                );
+            {Sql.end()}
+            """
+        )
+        return insert_statement
+
+    def _update_ddl(table_name) -> DDL:
+        update_statement = DDL(
+            f"""
+            {Trigger.create()} {table_name}{Trigger.name_update()}
+            {Trigger.after_update()} {table_name}
+            {Sql.begin()}
+                {Sql.delete()} {Search.table_name()}
+                {Sql.where()}
+                    {C.hash()} = {Sql.old()}.{C.hash()} {Sql.AND.value}
+                    table_name = '{table_name}';
+                {Sql.insert()} {Search.table_name()}(
+                    {C.hash()}, {C.table_name()}, {C.value()}
+                )
+                {Sql.values()} (
+                    {Sql.new()}.{C.hash()},
+                    '{table_name}',
+                    {Sql.new()}{C.value()}
+                );
+            {Sql.end()}
+            """
+        )
+        return update_statement
+
+
+    def _delete_ddl(table_name) -> DDL:
+        delete_statement = DDL(
+            f"""
+            {Trigger.create()} {table_name}{Trigger.name_update()}
+            {Trigger.after_delete()} {table_name}
+            {Sql.begin()}
+                {Sql.delete()} {Search.table_name()} {Sql.where()}
+                    {C.hash()} = {Sql.old()}.{C.hash()} {Sql.AND.value}
+                    {C.table_name()} = '{table_name}';
+            {Sql.end()}
+            """
+        )
+        return delete_statement
+
+    for table_name, table in metadata.tables.items():
+        insert_statement = _insert_ddl(table_name)
+        update_statement = _update_ddl(table_name)
+        delete_statement = _delete_ddl(table_name)
+        for trigger, statement in zip(
+            [Search.INSERT.value, Search.UPDATE.value, Search.DELETE.value],
+            [insert_statement, update_statement, delete_statement]
+        ):
+            event.listen(
+                table,
+                trigger,
+                statement.execute_if(dialect=Search.DIALECT.value)
+            )
 
 
 def create_view_all_table(
     engine_detail: EngineAttributes = None,
     metadata_detail: MetaAttributes = None,
+    session_detail: SessionAttributes = None
 ) -> None:
     """Prepare the database tables as view for a full-text search"""
-    if engine_detail or metadata_detail is None:
+    if engine_detail or metadata_detail or session_detail is None:
         raise ValueError('details missing error: TODO')
 
-    if engine_detail.db_name != metadata_detail.db_name:
+    if (engine_detail.db_name != metadata_detail.db_name or
+            engine_detail.db_name != session_detail.db_name or
+            metadata_detail.db_name != session_detail.db_name):
         raise ValueError('database must be the same error: TODO')
-    pass
 
+    db_name = engine_detail.db_name
+    metadata = metadata_detail.metadata
+    session = session_detail.session
+    table_names = [table_name for table_name in mk_table_names(db_name)]
 
-
-def mk_value_view_tables(db_name: str, metadata: MetaData) -> MetaData:
-    """"""
-    for table_name in :
-        Table(
-            table_name,
-            metadata,
-            *[column for column in mk_value_view_columns()]
+    with session() as mk_view_session:
+        select_table1 = select(
+            metadata.tables[table_names[0]].key_hash,
+            text(f"'{table_names[0]}'").label(f"{C.TABLE_NAME.value}"),
+            metadata.tables[table_names[0]].value
         )
-    return metadata
+        select_table2 = select(
+            metadata.tables[table_names[1]].key_hash,
+            text(f"'{table_names[1]}'").label(f"{C.TABLE_NAME.value}"),
+            metadata.tables[table_names[1]].value
+        )
+        select_table3 = select(
+            metadata.tables[table_names[2]].key_hash,
+            text(f"'{table_names[2]}'").label(f"{C.TABLE_NAME.value}"),
+            metadata.tables[table_names[2]].value
+        )
+        select_table4 = select(
+            metadata.tables[table_names[3]].key_hash,
+            text(f"'{table_names[3]}'").label(f"{C.TABLE_NAME.value}"),
+            metadata.tables[table_names[3]].value
+        )
+        unite_tables = union_all(
+            select_table1,
+            select_table2,
+            select_table3,
+            select_table4
+        )
+        create_view_statement = text(
+            f"""
+            {View.CREATE.value} {View.ALL_VALUES.value} {View.AS.value}'
+            """
+            ).columns(
+                *[column for column in mk_value_view_columns()]
+        )
+        mk_view_session.execute(create_view_statement, unite_tables)
+        mk_view_session.commit()
 
 
 def mk_value_view_columns() -> Generator[Column, None, None]:
-    """"""
+    """Make and yield each column for a Value View table"""
     for column in (
         mk_value_view_hash_column(),
         mk_value_view_tablename_column(),
@@ -78,27 +247,27 @@ def mk_value_view_columns() -> Generator[Column, None, None]:
 
 
 def mk_value_view_hash_column() -> Column:
-    """"""
+    """Create and return a key_hash column for the view table"""
     hash_key_column = ValueSearchColumn(
-        name = Col.HASH.value,
+        name = C.HASH.value,
         type_ = Conf.STR.value
     )
     return build_value_column(hash_key_column)
 
 
 def mk_value_view_tablename_column() -> Column:
-    """"""
+    """Create and return a tablename column for the view table"""
     table_name_column = ValueSearchColumn(
-        name = Col.TABLE_NAME.value,
+        name = C.TABLE_NAME.value,
         type_ = Conf.STR.value
     )
     return build_value_column(table_name_column)
 
 
 def mk_value_view_value_column() -> Column:
-    """"""
+    """Create and return a value column for the view table"""
     value_column = ValueSearchColumn(
-        name = Col.TABLE_NAME.value,
+        name = C.DATA_VALUE.value,
         type_ = Conf.STR.value
     )
     return build_value_column(value_column)
