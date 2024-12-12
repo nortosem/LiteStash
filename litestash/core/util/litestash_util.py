c7cdd5cb80c835499dbacdd6c767bc446cfdada7
nortosem <82008236+nortosem@users.noreply.github.com>
2024-12-12 04:18:41 +0000

    updated error check, docs, added threading, minor fixes
    FossilOrigin-Name: 57845533ac7369d50b9d33cddd6fb6f9b3facca3ea956ce4f10b9fbebbeb7a90

--- litestash/core/util/litestash_util.py
+++ litestash/core/util/litestash_util.py
@@ -22,38 +22,53 @@ Functions:
 - `get_values`: Retrieves all values from a table.
 
 """
-import re
 import orjson
-from sqlalchemy.orm.session import sessionmaker
-from sqlalchemy import create_engine
+from queue import Queue
+from pathlib import Path
+from hashlib import blake2b
+from typing import Callable
+from threading import Lock
+from threading import Thread
+from hashlib import blake2b
+from datetime import datetime
+from secrets import base64
+from secrets import SystemRandom
+from collections import namedtuple
+from pydantic import StrictStr
+from pydantic import StrictBytes
 from sqlalchemy import event
 from sqlalchemy import select
 from sqlalchemy import inspect
 from sqlalchemy import Engine
 from sqlalchemy import MetaData
 from sqlalchemy import Table
+from sqlalchemy import create_engine
 from sqlalchemy.orm.session import Session
-from collections import namedtuple
-from pathlib import Path
-from datetime import datetime
-from hashlib import blake2b
-from secrets import base64
-from secrets import SystemRandom
+from sqlalchemy.orm.session import sessionmaker
+from concurrent.futures import Future
 from litestash.logging import ENV
 from litestash.logging import root_logger as logger
 from litestash.models import LiteStashData
 from litestash.models import LiteStashStore
+from litestash.core.schema import Metadata
+from litestash.core.session import Session
 from litestash.core.config.root import Log
+from litestash.core.config.litestash_conf import Key
+from litestash.core.config.litestash_conf import Utils
+from litestash.core.config.litestash_conf import TaskAttr
+from litestash.core.config.litestash_conf import TaskSlots
 from litestash.core.config.litestash_conf import EngineAttr
 from litestash.core.config.litestash_conf import MetaAttr
 from litestash.core.config.litestash_conf import SessionAttr
 from litestash.core.config.litestash_conf import TimeAttr
 from litestash.core.config.litestash_conf import EngineConf
-from litestash.core.config.litestash_conf import Utils
 from litestash.core.config.schema_conf import Pragma
 from litestash.core.config.schema_conf import Sql
 from litestash.core.config.schema_conf import ColumnFields as C
 from litestash.core.util.schema_util import mk_tables
+from litestash.core.util.misc_util import name_match
+from litestash.core.util.misc_util import spaces_match
+
 
 def set_pragma(db_connection, connect):
     """Sets SQLite PRAGMA settings on a new connection.
@@ -99,7 +114,7 @@ def set_begin(db_connection):
     db_connection.exec_driver_sql(Sql.BEGIN.value)
 
 
-def setup_engine(db_name: str, data_path: str = None) -> Engine:
+def setup_engine(db_name: StrictStr, data_path: StrictStr = None) -> Engine:
     """Sets up a SQLAlchemy engine for the given database.
 
     Args:
@@ -109,16 +124,14 @@ def setup_engine(db_name: str, data_path: str = None) -> Engine:
         EngineAttributes: A namedtuple containing the database name and the
         engine.
     """
-    space = re.compile(r'\s+')
-    file_name = re.compile(f'\w+', flags=re.A)
     if db_name is None:
         logger.error(f'Valid database name required')
         raise ValueError(f'Database name missing')
-    if not file_name.match(db_name):
-        logger.error(f'')
+    if not name_match(db_name):
+        logger.error(f'Invalid file name {db_name}. ASCII only')
         raise ValueError(f'{EngineConf.db_name_ascii()}: {db_name}')
-    if space.match(db_name):
-        logger.error(f'')
+    if spaces_match(db_name):
+        logger.error(f'No space characters permitted in file name: {db_name}')
         raise ValueError(f'{EngineConf.db_name_space()}: {db_name}')
     if len(db_name) < EngineConf.min_name_length():
         logger.error(f'')
@@ -127,7 +140,7 @@ def setup_engine(db_name: str, data_path: str = None) -> Engine:
         logger.error(f'')
         raise ValueError(f'{EngineConf.db_name_length()}: {db_name}')
 
-    if data_path is None
+    if data_path is None:
         data_path = Path(
             f'{EngineConf.dirname()}/{db_name}'
         )
@@ -146,6 +159,7 @@ def setup_engine(db_name: str, data_path: str = None) -> Engine:
             raise
         except Exception as path_error:
             logger.error(f'{EngineConf.dir_path_error()}: {path_error}')
+            raise
 
     data_path.mkdir(parents=True, exist_ok=True)
     logger.debug(f'data_path mkdir if needed')
@@ -201,6 +215,14 @@ def setup_metadata(engine_stash: EngineAttributes):
         MetaAttributes: A namedtuple containing the database name and the
         initialized `MetaData` object.
     """
+    if engine_stash is None:
+        logger.error('Database engine attributes are missing')
+        raise(ValueError(f'Engine attributes cannot be None'))
+
+    if not isinstance(engine_stash, EngineAttributes):
+        logger.error(f'{type(engine_stash)}: invalid EngineAttributes')
+        raise(TypeError(f'Invalid engine attributes: {engine_stash}'))
+
     metadata = MetaData()
     logger.debug(f'{engine_stash.db_name} init {metadata}')
     metadata = mk_tables(engine_stash.db_name, metadata)
@@ -242,6 +264,14 @@ def setup_sessions(engine_stash: EngineAttributes):
     Raises:
         ValueError: If no tables are found in the database.
     """
+    if engine_stash is None:
+        logger.error('Session engine attributes missing')
+        raise(ValueError(f'EngineAttributes cannot be None'))
+
+    if not isinstance(engine_stash, EngineAttributes):
+        logger.error(f'{type(engine_stash)}: invalid type')
+        raise(TypeError(f'EngineAttributes not found: {engine_stash}'))
+
     if inspect(engine_stash.engine).get_table_names():
         logger.debug(f'sessionmaker called for {engine_stash.engine}')
         session = sessionmaker(engine_stash.engine)
@@ -266,7 +296,122 @@ SessionAttributes = namedtuple(
 SessionAttributes.__doc__ = SessionAttr.DOC.value
 
 
-def allot(size: int = 6) -> str:
+def create_task(session: Session, queue, Queue): -> Task
+    """Create a Task
+
+    Function for creating a task to manage a specific database.
+
+    Args:
+        session (Session):
+        queue (Queue):
+    Returns:
+        task (Task):
+    Raises:
+
+    """
+    class Task:
+        """Task
+
+        This class manages the creation and access to task runners for each
+        database thread.
+
+        Attributes:
+
+            __slots__ (tuple): A tuple of attribute names for memory
+                optimization.
+
+        Methods:
+            __run:
+            send:
+            stop:
+        """
+        __slots__ = TaskSlots.get()
+
+        def __init__(self, session: Session, queue: Queue):
+            """Task Constructor
+
+            Initialize the thread Task and link it to a database.
+            """
+            self.db_name = session.db_name
+            self.session = session.session
+            self.queue = queue
+            self.thread = Thread(target=self.__run)
+            self.thread.daemon = True
+            self.thread.start()
+            self.lock = Lock()
+
+
+        def __run(self, func, data, future: Future):
+            """__Run
+
+            The Task thread's internal work function processor
+            """
+            while True:
+                work is None:
+                    break
+                try:
+                    with self.lock:
+                        try:
+                            func, data, future = work
+                            result = func(data)
+                            future.set_result(result)
+                        except Exception as error:
+                            future.set_exception(error)
+                except Exception as error:
+                    logger.error(f'Task error: {error}')
+                finally:
+                    self.queue.task_done()
+
+
+        def send(self, func: Callable, data: LiteStashData, future=None):
+            """Send
+
+            Send a job for the task to run in the queue for its database.
+
+            Args:
+                func (Callable): The API function to call
+                data (LiteStashData): The given key and, or, value.
+                future (Future): The Future object for this task.
+            """
+            self.queue.put((func, data, future))
+
+
+        def stop(self):
+            """Stop
+
+            Top the thread for this database.
+            """
+            self.queue.put(None)
+            self.queue.join()
+
+
+    return Task(session, queue)
+
+
+def setup_tasks(session: Session, queue: Queue): -> TaskAttributes
+    """Creates and returns Task and returns it for a specific database.
+
+    Args
+
+
+    Returns:
+        TaskAttributes:
+
+    Raises:
+
+    """
+
+
+TaskAttributes = namedtuple(
+    TaskAttr.TYPE_NAME.value,
+    [
+        TaskAttr.DB_NAME.value,
+        TaskAttr.TASK.value
+    ]
+)
+TaskAttributes.__doc__ = TaskAttr.DOC.value
+
+def allot(size: int = 6) -> StrictStr:
     """Generates a unique random string for key distribution.
 
     Args:
@@ -275,6 +420,14 @@ def allot(size: int = 6) -> str:
     Returns:
         A URL-safe Base64-encoded string of the specified size.
     """
+    if size is None:
+        logger.error('No size provided for the random bytes')
+        raise(ValueError('A value of six or more is required'))
+
+    if not isinstance(size, int):
+        logger.error(f'Integer size not provided: {size}')
+        raise(TypeError(f'Invalid size type: {type(size)}'))
+
     if size < 6:
         raise ValueError('min size')
 
@@ -285,7 +438,7 @@ def allot(size: int = 6) -> str:
     return base64.urlsafe_b64encode(lot).decode()
 
 
-def digest_key(key: str) -> bytes:
+def digest_key(key: StrictStr) -> bytes:
     """Generates a bytes digest of the given key.
 
     Args:
@@ -294,20 +447,67 @@ def digest_key(key: str) -> bytes:
     Returns:
         bytes: The digest of the key in bytes.
     """
-    digest = blake2b(key.encode(), digest_size=Utils.SIZE.value).digest()
-    return digest
+    if key is None:
+        logger.error('No string provided for the key')
+        raise ValueError('Key cannot be empty')
+
+    if spaces_match(key):
+        logger.error('Spaces found in digest key string argument')
+        raise ValueError(f'Invalid key provided: {key}')
+
+    if not name_match(key):
+        logger.error('Valid key requires ASCII only.')
+        raise ValueError(f'')
+
+    try:
+        digest = blake2b(
+            key.encode(),
+            digest_size=Utils.SIZE.value
+        ).digest()
+        return digest
+    except ValidationError as key_error:
+        logger.error()
+        raise TypeError from key_error
 
 
-def mk_hash(digest: bytes) -> str:
+def mk_hash(digest: StrictBytes) -> StrictStr:
     """Generates a URL-safe Base64 hash of the given key digest."""
-    return base64.urlsafe_b64encode(digest).decode()
+    if digest is None:
+        logger.error('Digest required to make a hash')
+        raise ValueError(f'No digest provided')
+
+    if not isinstance(digest, bytes):
+        logger.error(f'Digest type is bytes, not {type(digest)}')
+        raise TypeError(f'Invalid type of digest: {type(digest)}')
+
+    try:
+        return base64.urlsafe_b64encode(digest).decode()
+    except ValidationError as digest_error:
+        logger.error()
+        raise TypeError from digest_error
 
 
-def get_primary_key(key: str) -> str:
+def get_primary_key(key: StrictStr) -> StrictStr:
     """Generates a unique primary key for a given key."""
-    key_digest = digest_key(key)
-    keyed = mk_hash(key_digest)
-    return keyed
+    if key is None:
+        logger.error('Primary key cannot be empty')
+        raise ValueError('No primary key string provided')
+
+    if spaces_match(key):
+        logger.error(f'Primary key contains spaces: {key}')
+        raise ValueError(f'Invalid characters found in key: {key}')
+
+    if not name_match(key):
+        logger.error('Primary key contains non-ASCII characters')
+        raise ValueError(f'Invalid characters in key: {key}')
+
+    try:
+        key_digest = digest_key(key)
+        keyed = mk_hash(key_digest)
+        return keyed
+    except ValidationError as key_error:
+        logger.error(Key.log_error())
+        raise TypeError(Key.type_error()) from value_error
 
 
 def get_time() -> tuple[int, int]:
@@ -318,7 +518,6 @@ def get_time() -> tuple[int, int]:
     now = GetTime(store_timestamp, store_ms)
     return now
 
-#        time (GetTime): The GetTime namedtuple record of key creation.
 GetTime = namedtuple(
     TimeAttr.TYPE_NAME.value,
     [
@@ -338,6 +537,10 @@ def get_datastore(data: LiteStashData) -> LiteStashStore:
     Returns:
         A `LiteStashStore` object ready for database storage.
     """
+    if data is None:
+        logger.error('No LiteStash Data found')
+        raise ValueError(f'')
+
     primary_key = get_primary_key(data.key)
     logger.debug(f'get_datastore primary_key: {primary_key}')
     now = get_time()
@@ -357,7 +560,7 @@ def get_datastore(data: LiteStashData) -> LiteStashStore:
     return stash_data
 
 
-def get_keys(session: Session, table: Table) -> list[str]:
+def get_keys(session: Session, table: Table) -> list[StrictStr]:
     """Retrieves all keys from the specified table.
 
     Args:
@@ -367,6 +570,22 @@ def get_keys(session: Session, table: Table) -> list[str]:
     Returns:
         list[str]: A list of all keys in the table.
     """
+    if session is None:
+        logger.error()
+        raise ValueError()
+
+    if table is None:
+        logger.error()
+        raise ValueError()
+
+    if not isinstance(session, Session):
+        logger.error()
+        raise(TypeError())
+
+    if not isinstance(table, Table):
+        logger.error()
+        raise(TypeError())
+
     with session() as keys_get:
         sql_statement = select(table.c[C.KEY.value])
         keys = keys_get.execute(sql_statement).scalars().all()
@@ -383,6 +602,22 @@ def get_values(session: Session, table: Table) -> list[dict]:
     Returns:
         list[dict]: A list of all JSON values in the table (deserialized).
     """
+    if session is None:
+        logger.error()
+        raise ValueError()
+
+    if table is None:
+        logger.error()
+        raise ValueError()
+
+    if not isinstance(session, Session):
+        logger.error()
+        raise TypeError()
+
+    if not isinstance(table, Table):
+        logger.error()
+        raise TypeError()
+
     with session() as values_get:
         sql_statement = select(table.c[C.VALUE.value])
         values = values_get.execute(sql_statement).scalars().all()
