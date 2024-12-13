cfeafb034309a713bf935f4eb4dd2cb7792ad006
nortosem <82008236+nortosem@users.noreply.github.com>
2024-12-13 22:58:50 +0000

    implementation of asyncio task for sqlite databases, added comments
    FossilOrigin-Name: 50076ddebff56c893107c6ae8b44777eaa226ee6d2937a3cb64c411fc0e6dda0

--- litestash/core/util/task_util.py
+++ litestash/core/util/task_util.py
@@ -1,22 +1,30 @@
 """Task Utility Module
 
-The task module defines the utility functions for managing threads with the
-sqlite database files for LiteStash.
+This module provides utilities for managing asynchronous tasks related to
+SQLite database operations within the LiteStash system. It uses asyncio
+for handling concurrent database access across different threads.
 
 Functions:
 
-- `create_task`:
 - `setup_tasks`:
+  Creates and returns a `TaskAttributes` namedtuple containing a task
+  instance for a specific database.
 
+Classes:
+
+- `Task`: Manages the execution of database operations in an asynchronous,
+  thread-safe manner for each database.
 """
-from queue import Queue
+import asyncio
+from asyncio import Lock
+from asyncio import Future
+from typing import Any
+from typing import Tuple
 from typing import Callable
-from threading import Lock
-from threading import Thread
+from pydantic import StrictBool
 from collections import namedtuple
 from litestash.models import LiteStashData
 from litestash.logging import root_logger as logger
-from litestash.core.config.task_conf import TaskConf
 from litestash.core.config.task_conf import TaskAttr
 from litestash.core.config.task_conf import TaskSlots
 from litestash.core.util.core_util import SessionAttributes
@@ -26,98 +34,99 @@ class Task:
     """Task
 
     This class manages the creation and access to task runners for each
-    database thread.
+    SQLite database, ensuring thread-safe operations using asyncio.
 
     Attributes:
-
-        db_name (str):
-        session (Session):
-        queue (Queue):
-        thread (Tread):
-        lock (Lock):
+        db_name (str): The name of the database this task is associated with.
+        session (Session): The session object for database operations.
+        lock (Lock): An asyncio lock for managing concurrent access.
+        loop (asyncio.AbstractEventLoop): The event loop for scheduling tasks.
 
     Methods:
-        __run:
-        send:
-        stop:
+        __run: Private async method to run tasks in a thread-safe manner.
+        send: Public method to schedule tasks in the event loop.
     """
-    __slots__ = TaskSlots.get()
+    __slots__ = TaskSlots.slots()
 
-    def __init__(self, session: SessionAttributes):
+    def __init__(self,
+        session: SessionAttributes,
+        new_loop: StrictBool = False
+    ):
         """Task Constructor
 
         Initialize the thread Task and link it to a database.
+
+        Args:
+            session (SessionAttributes): Session attributes for database
+                interaction.
+            new_loop (StrictBool): If True, creates a new event loop;
+                otherwise, uses the running one.
         """
         self.db_name = session.db_name
         self.session = session.session
-        self.queue = Queue(maxsize=TaskConf.MAX_QUEUE.value)
-        self.thread = Thread(target=self.__run)
-        self.thread.daemon = True
-        self.thread.start()
         self.lock = Lock()
 
+        def _mk_loop(self):
+            self.loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(self.loop)
 
-    def __run(self):
+        if not new_loop:
+            try:
+                self.loop = asyncio.get_running_loop()
+            except RuntimeError:
+                logger.warning('No running event loop found; creating new one.')
+                _mk_loop(self)
+        else:
+            _mk_loop(self)
+
+
+    async def __run(self, work: Tuple[Callable, LiteStashData]) -> Any:
         """Run
 
-        The Task thread's internal work function processor
-        """
-        while True:
-            work = self.queue.get()
-            if work is None:
-                break
-            try:
-                with self.lock:
-                    func, data, future = work
-                    result = func(data)
-                    future.set_result(result)
-            except Exception as unknown_error:
-                future.set_exception(unknown_error)
-                logger.error('Task for %s raised error: %s',
-                    func.__name__, unknown_error)
-                raise
-            finally:
-                self.queue.task_done()
-
-
-    def send(self, func: Callable, data: LiteStashData, future=None):
-        """Send
-
-        Send a job for the task to run in the queue for its database.
+        The Task thread work function processor. Execute the api function in a
+        thread-safe manner.
 
         Args:
-            func (Callable): The API function to call
-            data (LiteStashData): The given key and, or, value.
-                future (Future): The Future object for this task.
+            work (Tuple): A tuple containing the api function used to process
+                LiteStash data.
+
+        Returns:
+            result (Any): The result of the function execution.
+
+        Raises:
+            Exception: Log and raise any errors during task execution.
         """
-        self.queue.put((func, data, future))
+        try:
+            async with self.lock:
+                func, data = work
+                result = await asyncio.to_thread(func, data)
+                return result
+        except Exception as unknown_error:
+            logger.error('Task for %s raised error: %s',
+                func.__name__, unknown_error)
+            raise
 
 
-    def stop(self):
-        """Stop
-        Top the thread for this database.
+    def send(self, func: Callable, data: LiteStashData) -> Future:
+        """Send
+
+        Schedule a job for the task to run in the context of its database's
+        event loop.
+
+        Args:
+            func (Callable): The function to call, expecting LiteStashData as an
+                argument.
+            data (LiteStashData): The LiteStash data (key and/or value) to
+                process.
+
+        Returns:
+            Future: A future representing the result of the task.
         """
-        self.queue.put(None)
-        self.queue.join()
-
-
-def setup_tasks(session: SessionAttributes) -> TaskAttributes:
-    """Creates and returns Task and returns it for a specific database.
-
-    Args:
-
-        db_name (StrictStr): Name of of the database being threaded
-
-    Returns:
-
-        TaskAttribute:
-
-    Raises:
-
-    """
-    task = Task(session)
-    task = TaskAttributes(session.db_name, task)
-    return task
+        work = (func, data)
+        future = asyncio.run_coroutine_threadsafe(
+            self.__run(work), self.loop
+        )
+        return future
 
 
 TaskAttributes = namedtuple(
@@ -128,3 +137,23 @@ TaskAttributes = namedtuple(
     ]
 )
 TaskAttributes.__doc__ = TaskAttr.DOC.value
+
+
+def setup_tasks(session: SessionAttributes) -> TaskAttributes:
+    """Creates and returns TaskAttributes for a specific database.
+
+    This function initializes a Task for the given session, encapsulating
+    database-specific task management.
+
+    Args:
+        session (SessionAttributes): The session attributes for the database
+            to set up.
+
+    Returns:
+        TaskAttributes: A namedtuple containing the database name and
+            the corresponding Task instance.
+
+    """
+    task = Task(session)
+    task = TaskAttributes(session.db_name, task)
+    return task
