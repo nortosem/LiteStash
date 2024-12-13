94e9e3ad36f7c996b32c62cf72bf01765b16dbfa
jcy <>
2024-12-13 22:57:52 +0000

    updated config for asyncio tasks,added docs
    FossilOrigin-Name: 9317176717955cc69e8cce5c95bed78087217a1a3657bffc4596547ced4efa43

--- litestash/core/config/task_conf.py
+++ litestash/core/config/task_conf.py
@@ -3,23 +3,18 @@
 
 """
 from litestash.core.config.root import Valid
-from litestash.core.config.litestash_util import EngineAttr
-from litestash.core.config.litestash_util import SessionAttr
-
-class TaskConf(Valid):
-    """The Configuration valudes for a Task"""
-    MAX_QUEUE = 8
+from litestash.core.config.litestash_conf import EngineAttr
+from litestash.core.config.litestash_conf import SessionAttr
 
 
 class TaskSlots(Valid):
     """The attribute Slots for a Task thread."""
     DB_NAME = f'{EngineAttr.DB_NAME.value}'
     SESSION = f'{SessionAttr.SESSION.value}'
-    QUEUE = 'queue'
     LOCK = 'lock'
 
     @staticmethod
-    def get():
+    def slots():
         """Get the slots for a Task"""
         return tuple([slot.value for slot in TaskSlots])
 
@@ -30,4 +25,12 @@ class TaskAttr(Valid):
     DB_NAME = f'{EngineAttr.DB_NAME.value}'
     TASK = 'task'
     VALUE_ERROR = 'todo'
-    DOC = '''todo'''
+    DOC = '''
+        A namedtuple representing task attributes for a specific database:
+
+            - db_name (str): The name of the database.
+            - task (Task): The Task instance associated with this database.
+
+        This structure helps in managing tasks alongside their respective
+        databases.
+    '''
