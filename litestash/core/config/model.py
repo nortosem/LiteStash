ba3fa4c3221bf1c2b55582f887b7656ce37e1126
nortosem <82008236+nortosem@users.noreply.github.com>
2024-12-14 06:02:32 +0000

    minor fixes, setup async for testing, 100x slower with async?!!
    FossilOrigin-Name: cfbff121100ad96b230ad6acb59fd5d3586fb07312235cc0e7e7317650be033d

--- litestash/core/config/model.py
+++ litestash/core/config/model.py
@@ -3,13 +3,39 @@
 Provides Pydantic model configuration constants for serialization and behavior.
 """
 from litestash.core.config.root import Valid
+from litestash.core.config.litestash_conf import DataScheme
 
-class StashConf(Valid):
-    """LiteStash Model Config
+class Parameter(Valid):
+    """LiteStash Model Parameters
 
-    Enumeration for the model_config dictionary slots/keys.
+    Enumeration parameters used in LiteStash models.
     """
-    ORM_MODE = 'orm_mode'
+    SLOTS = True
+    FROM_ATTRIBUTES = 'from_attributes'
     EXTRA = 'extra'
-    JSON_LOADS = 'json_loads'
-    JSON_DUMPS = 'json_dumps'
+
+
+class StashDataclass(Valid):
+    """LiteStash Dataclass Config Parameters
+
+    Enumeration of ConfigDict parameters used for pydantic dataclass.
+    """
+    CONFIG = ((Parameter.FROM_ATTRIBUTES.value,
+               DataScheme.FROM_ATTRIBUTES.value),
+              (Parameter.EXTRA.value,
+               DataScheme.FORBID_EXTRA.value)
+              )
+
+
+class StashField(Valid):
+    """LiteStashData Key Config
+
+
+    """
+    VALID_KEY_ASCII = 'Value error, ASCII keys only'
+    VALID_KEY_TEXT = 'Key text missing'
+    VALID_VALUE_JSON = 'JSON input should be string, bytes or bytearray'
+    VALID_VALUE_TYPE = 'Input should be a valid string'
+    VALID_KEY_LENGTH = '1 validation error for'
+    AT_MOST = 'String should have at most 999 characters'
+    AT_LEAST = 'String should have at least 3 characters'
